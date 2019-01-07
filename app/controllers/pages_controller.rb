c03012565e8b360599bbb21200f991bcd1ab857d
AREEBAISHTIAQ <areeba.aic@gmail.com>
2019-01-07 18:34:14 +0500

    added signups

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @user = User.new
   end
 
   def about
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You have signed up successfully!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
-def show
-  @user = User.find(params[:id])
-end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params 
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :username, :phone_number, :bio, :location, :birthday)
+  end
 end

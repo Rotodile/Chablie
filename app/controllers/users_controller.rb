189c66313adc42680301000fa3bc9b26058c4b83
AREEBAISHTIAQ <areeba.aic@gmail.com>
2019-01-13 18:48:16 +0500

    email test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     if @user.save
       UserMailer.account_activation(@user).deliver_now
       flash[:notice] = "Please verify your email to continue!"
-      redirect_to @user
+      redirect_to user_path
     else
       render 'new'
     end

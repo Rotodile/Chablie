199f61cdc8456b7da675b6485d5db1fecfc61636
AREEBAISHTIAQ <areeba.aic@gmail.com>
2019-01-14 15:08:34 +0500

    removed something

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :users
   get '/about', to: 'pages#about'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
+  resources :users
   resources :account_activations, only: [:edit]
 end

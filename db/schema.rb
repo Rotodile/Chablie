7836cf23e5485152d5270abf9f162796447c61a9
alexmalus <malus.alexandru@yahoo.com>
2019-10-05 23:06:45 +0200

    schema state after running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_143641) do
+ActiveRecord::Schema.define(version: 2019_05_27_105106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,7 +140,6 @@ ActiveRecord::Schema.define(version: 2019_05_31_143641) do
     t.string "chable_picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "chables", "users"
